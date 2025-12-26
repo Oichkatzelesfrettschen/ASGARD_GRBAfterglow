590ec3053bc1563b268a32a955a862654a5c5331
eirikr <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-12-26 09:39:32 -0800

    feat: integrate shared library (Phase 4)

--- extinc.py
+++ extinc.py
@@ -1,12 +1,18 @@
-from extinction import fitzpatrick99 as f99
+from grb_common.extinction import extinction_curve
+from grb_common.constants import C
 import numpy as np
 
 def opt_extinction(mag_data,mag_err,frequency,Rv,Ebv,zeropointflux):
     
-    wave = np.array([2.997e10/frequency*1e8])
+    # C is speed of light in cm/s (from grb_common)
+    # Convert frequency (Hz) to wavelength (Angstrom)
+    wave = np.array([C/frequency*1e8])
     Av = Rv * Ebv
     
-    mag_data_deredden = mag_data - f99(wave, Av, Rv)
+    # Calculate extinction using grb_common
+    ext_mag = extinction_curve(wave, av=Av, rv=Rv, law='Fitzpatrick99')
+    
+    mag_data_deredden = mag_data - ext_mag
     flux_data_deredden = 10**(0.4*(zeropointflux-mag_data_deredden))
     flux_data_err = 0.4*np.log(10.0)*flux_data_deredden*mag_err
     
